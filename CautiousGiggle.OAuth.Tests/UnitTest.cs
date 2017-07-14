cc1d9d181ede23884b886393347c0bf49a907a2f
benjamin ketron <=>
2017-07-14 16:32:13 -0600

    basic project structure

---
+++ CautiousGiggle.OAuth.Tests/UnitTest.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using Xunit;
+
+namespace CautiousGiggle.OAuth.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            Assert.Equal(true, true);
+        }
+    }
+}
